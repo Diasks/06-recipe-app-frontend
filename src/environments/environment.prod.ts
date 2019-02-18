70825b031dee462719928060863c645c04f46579
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 16:54:48 +0100

    add url to api with key, id and url-address to be able to access secret information in searchservice functions when making api-requests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiRoot: "http://api.yummly.com/v1/api/recipes?",
+  apiRootId: "http://api.yummly.com/v1/api/recipe/",
+  apiId:  "dba541dd",
+  apiKey: "48390d4236721de97f3673b31bd4f52c"
 };
