31242474cffe5b885685de6dde34bdc223127376
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 16:26:51 +0100

    Add model for recipe items

---
+++ src/app/Searchitem.model.ts
@@ -0,0 +1,11 @@
+export class SearchItem {
+    constructor(
+      public recipeName: string,
+      public course: string,
+      public url: string,
+      public smallImageUrls: string,
+      public id: string,
+      public ingredients: []
+    ) {}
+  }
+  
