b871b1343ba4826ec41805a74e4cdc97ed87a960
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 16:30:11 +0100

    make component for recipe-details that will display the details of a specific recipe when clicked on

---
+++ src/app/recipe-details/recipe-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core";
+import { SearchItem } from "../Searchitem.model";
+import { SearchService } from "../search-service.service";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: "app-recipe-details",
+  templateUrl: "./recipe-details.component.html",
+  styleUrls: ["./recipe-details.component.css"]
+})
+export class RecipeDetailsComponent implements OnInit {
+  recipe$;
+  // : Observable<SearchItem[]>;
+  recipeId;
+
+  constructor(private service: SearchService, private route: ActivatedRoute) {
+    this.recipeId = this.route.snapshot.paramMap.get("id");
+  }
+
+  ngOnInit(): void {
+    this.getRecipeId();
+  }
+
+  getRecipeId() {
+    this.service.getRecipeId(this.recipeId).subscribe(data => {
+      return this.recipe$ = data;
+    });
+  }
+
+  saveRecipe(recipeId:string) {
+    const listId = 1;
+this.service.addRecipeToList(listId, recipeId);
+  }
+
+  
+  // getRecipeId(): void
+  // {
+
+  //   const recipeId = +this.route.snapshot.paramMap.get('id');
+  //   this.service.getRecipeId(recipeId).subscribe(recipes$ => this.recipes$=recipes$);
+  //   };
+
+  // this.recipes$ = this.service.getRecipeId(recipeId);
+}
