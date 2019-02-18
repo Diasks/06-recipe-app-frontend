a03bbdb435c008e1f7ae0279102addd3b11f82e8
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-18 20:27:57 +0100

    make routes for recipe, list, login, register, logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RecipeDetailsComponent } from "./recipe-details/recipe-details.component";
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { SavedComponent } from './saved/saved.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SavedDetailsComponent } from './saved-details/saved-details.component';
+import { CreateListComponent } from './create-list/create-list.component';
+// import { PageNotFoundComponent} from './not.found-component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: RecipeListComponent },
+  { path: "recipe/:id", component: RecipeDetailsComponent },
+  { path: 'create-list', component: CreateListComponent},
+  { path: 'saved', component: SavedComponent},
+  { path: 'saved/:id', component: SavedDetailsComponent},
+ { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
