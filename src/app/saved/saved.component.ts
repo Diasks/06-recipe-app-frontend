7030580e43ae70db40f0bf66489c7d5f05f9d7a9
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-02-21 12:57:06 +0100

    make function on submit that will take the new title and id of list and update it

--- src/app/saved/saved.component.ts
+++ src/app/saved/saved.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search-service.service';
 import { Saved } from './saved.model';
 
+
 @Component({
   selector: 'app-saved',
   templateUrl: './saved.component.html',
@@ -29,4 +30,21 @@ this.savedService.removeRecipeList(listId)
  console.log(data);
 });
   }
+
+
+
+  onSubmit(event){
+    const target = event.target;
+    const title = target.querySelector('#title').value;
+    const list_id = target.querySelector('#list_id').value;
+    this.savedService.updateList(title, list_id).subscribe(data =>{
+   if(data) {
+    window.alert(`listan är uppdaterad och heter numera ${title}!`);
+   }
+    }) 
+  };
+
 }
+
+
+
