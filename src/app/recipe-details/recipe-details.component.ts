391e53478264ffeaf1c1a423c040482163f145bd
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-03-25 14:12:26 +0100

    make function for saving recipe to list and function to get lists, to be able to render out lists with saved recipes in recipe-details html on dropdown-button

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { SearchService } from "../search-service.service";
 import { ActivatedRoute } from "@angular/router";
-
+import { Saved } from "../saved/saved.model";
+import {Router} from "@angular/router";
+import { SearchItem } from '../Searchitem.model';
+import { List } from '../create-list/list.model';
 
 @Component({
   selector: "app-recipe-details",
@@ -11,8 +14,10 @@ import { ActivatedRoute } from "@angular/router";
 export class RecipeDetailsComponent implements OnInit {
   recipe$;
   recipeId;
+  recipe: SearchItem;
+  list: List[];
 
-  constructor(private service: SearchService, private route: ActivatedRoute) {
+  constructor(private service: SearchService, private route: ActivatedRoute, private router: Router) {
     this.recipeId = this.route.snapshot.paramMap.get("id");
   }
 
@@ -24,14 +29,37 @@ export class RecipeDetailsComponent implements OnInit {
     this.service.getRecipeId(this.recipeId).subscribe(data => {
       return (this.recipe$ = data);
     });
+    this.getLists();
   }
 
-  saveRecipe(recipeId: string, list_id) {
-    this.service.addRecipeToList(list_id, recipeId).subscribe(data => {
-      
-      window.alert(`${recipeId} är nu sparad i vald lista!`);
+  saveRecipe(recipeId: string, listId: number) {
+
+    debugger;
+    const recipeAdd = new Saved();
+    debugger;
+    recipeAdd.recipe = recipeId;
+    recipeAdd.listId = listId;
+    recipeAdd.email = this.service.getEmail();
+    debugger;
   
-      return data;
+    this.service.saveRecipe(recipeAdd).subscribe(result => {
+      debugger;
+      console.log(result);
+      this.router.navigateByUrl('/saved');
     });
   }
+
+  getLists(): void {
+    let email = this.service.getEmail();
+    this.service.getList(email).subscribe(data => {
+      let arr = [];
+      for (let i = 0; i < 100; i++){
+        if(data[i] != undefined) arr.push(data[i]);
+      }
+      this.list = arr;
+    })
+  }
+
+  
+
 }
