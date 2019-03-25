b1e7bf2d9ed605c91e1701e2047eaffe5da20be8
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-03-25 14:10:53 +0100

    add email to saved model instead of userid

--- src/app/saved/saved.model.ts
+++ src/app/saved/saved.model.ts
@@ -1,16 +1,7 @@
-import { SearchItem } from '../Searchitem.model';
-
 export class Saved {
-public id: number;
-public title; string;
-public recipe: SearchItem[];
-public user_id: number;
-
-constructor(id: number, title: string, recipe: SearchItem[], user_id: number){
-    this.id = id;
-    this.title = title;
-    this.recipe = recipe;
-    this.user_id = user_id;
-}
+ id: string;
+ recipe: string;
+ listId:number;
+ email: string;
 
 }
