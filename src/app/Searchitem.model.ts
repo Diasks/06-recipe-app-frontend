eda9f561efd54c80852aeeddaa4b14e04f6ad6a5
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-03-25 09:52:06 +0100

    add email to searchitem model

--- src/app/Searchitem.model.ts
+++ src/app/Searchitem.model.ts
@@ -1,11 +1,13 @@
 export class SearchItem {
     constructor(
       public recipeName: string,
+      public name: string,
       public course: string,
       public url: string,
       public smallImageUrls: string,
       public id: string,
-      public ingredients: []
+      public ingredients: [],
+      public email: string
     ) {}
   }
   
